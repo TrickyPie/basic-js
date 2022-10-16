c199108269e8b9dc6055f127416c439f803a482b
Trickypie <klimova.pr@gmail.com>
2022-10-17 00:29:00 +0300

    feat: seasons

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,10 +21,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class VigenereCipheringMachine {
   encrypt(message, key) {
-    if (message === undefined || key === undefined || ) {
+    /*if (message === undefined || key === undefined || ) {
       throw new Error('Incorrect arguments!');
     }
-    /* let alphabet = {
+     let alphabet = {
       'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25
     }
 
@@ -37,12 +37,12 @@ class VigenereCipheringMachine {
 
 
 
-    //throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError('Not implemented');
   }
   decrypt(crypt, key) {
-    if (crypt === undefined || key === undefined) {
+    /* if (crypt === undefined || key === undefined) {
       throw new Error('Incorrect arguments!');
-    }
+    } */
 
   }
 }
