f5846c8e4ac5541cb170c388c94751b675d6f087
Trickypie <klimova.pr@gmail.com>
2022-10-16 22:28:49 +0300

    feat: --double-next array array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  console.log(arr)
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  let compareArr = [
+    "--discard-next",
+    "--discard-prev",
+    "--double-next",
+    "--double-prev",
+  ]
+
+  let newArr = [...arr];
+  let resultArr = [...arr];
+
+  for (let i = 0; i < resultArr.length; i++) {
+    switch (arr[i]) {
+      case compareArr[0]:
+        newArr.splice(i + 1, 1);
+        break;
+
+      case compareArr[1]:
+        if (newArr[i - 2] === compareArr[0]) {
+          continue;
+        } else if (i !== 0) {
+          newArr.splice(i - 1, 1);
+        }
+        break;
+
+      case compareArr[2]:
+        if (i !== newArr.length - 1) {
+          newArr.splice(i + 1, 0, newArr[i + 1]);
+        }
+        break;
+
+      case compareArr[3]:
+        if (newArr[i - 2] === compareArr[0]) {
+          continue;
+        } else if (i !== 0) {
+          newArr.splice(i + 1, 0, newArr[i - 1]);
+        }
+        break;
+      default:
+    }
+  }
+
+  return newArr.filter((elem) => !compareArr.includes(elem));
+
+  /* --discard-next excludes the next element of the array from the transformed array.
+  --discard-prev excludes the previous element of the array from the transformed array.
+  --double-next duplicates the next element of the array in the transformed array.
+  --double-prev duplicates the previous element of the array in the transformed array. */
+
 }
 
 module.exports = {
